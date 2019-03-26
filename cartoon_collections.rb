783c0639fa8b9e165b90c4a7a9dd6413359b3481
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-26 21:55:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |call|
-    puts "#{call.&:capitalize}!"
+  planeteer_calls.map 
   end
 end
 
