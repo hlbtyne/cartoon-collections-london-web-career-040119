03d553941676f6fe6c4ca22d2b7be5ef3d366249
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-26 21:51:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,11 +10,11 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? do |call|
-    call.length > 4
-  end
-end
+# def long_planeteer_calls(planeteer_calls)
+#   planeteer_calls.any? do |call|
+#     call.length > 4
+#   end
+# end
 
 def find_the_cheese#
   cheese_types = ["cheddar", "gouda", "camembert"]
